f27586f2ad3d2fe58f3f15c896863e8be0d8e27d
Evangel <evangelopolot@gmail.com>
2023-10-06 14:44:10 +0100

    Test: purchaseTicket given number of tickets above the maximum, then an exception should be thrown

--- cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
+++ cinema-tickets-java/src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -3,6 +3,8 @@ package uk.gov.dwp.uc.pairtest;
 import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
+import java.util.HashMap;
+
 public class TicketServiceImpl implements TicketService {
     /**
      * Should only have private methods other than the one below.
@@ -12,6 +14,13 @@ public class TicketServiceImpl implements TicketService {
     public void purchaseTickets(Long accountId, TicketTypeRequest... ticketTypeRequests) throws InvalidPurchaseException {
         if(accountId <= 0){
             throw new InvalidPurchaseException();
+        }
+
+        for (TicketTypeRequest request : ticketTypeRequests) {
+            int quantity = request.getNoOfTickets();
+            if(quantity > 20){
+                throw new InvalidPurchaseException();
+            }
         }
     }
 
--- cinema-tickets-java/src/test/java/TicketServiceTest.java
+++ cinema-tickets-java/src/test/java/TicketServiceTest.java
@@ -5,12 +5,14 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 import uk.gov.dwp.uc.pairtest.TicketService;
 import uk.gov.dwp.uc.pairtest.TicketServiceImpl;
+import uk.gov.dwp.uc.pairtest.domain.TicketTypeRequest;
 import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class TicketServiceTest {
     private TicketService testService;
+    private TicketTypeRequest typeRequest;
 
     @BeforeEach
     public void setUp(){
@@ -25,5 +27,11 @@ public class TicketServiceTest {
         assertThrows(InvalidPurchaseException.class, () -> testService.purchaseTickets(input));
     }
 
+    @Test
+    @DisplayName("Test purchaseTicket given number of tickets above the maximum, then an exception should be thrown")
+    public void testPurchaseTicketsGivenValidIDAndInvalidTicketTypeThrowException() {
+        typeRequest = new TicketTypeRequest(TicketTypeRequest.Type.ADULT,21);
+        assertThrows(InvalidPurchaseException.class, () -> testService.purchaseTickets(1L, typeRequest));
+    }
 
 }
